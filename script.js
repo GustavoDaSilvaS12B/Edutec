ba523deb7e7d73f4e4720574398fe1e5666be039
EA1 - Gustavo Da Silva Souza <silva_gustavo03@educadventista.org>
2025-09-02 15:08:46 -0300

    Pequenas alterações

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Qual é o clima predominante do bioma Amazônia?",
+        answers: [
+            { id: 1, text: "Clima temperado", correct:false },
+            { id: 2, text: "Clima tropical úmido", correct:true },
+            { id: 3, text: "Clima desértico", correct:false },
+            { id: 4, text: "Clima polar", correct:false },
+        ],
+    },
+]
